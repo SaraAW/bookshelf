24a2498ba402de981c584b41b4060a01b89a3664
Sara Abdel Wahab <sara.abdelwahab@tele2.com>
2022-09-07 11:01:27 +0200

    ex1 extra credit 1

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,31 +1,38 @@
-// 🐨 you'll need to import react and createRoot from react-dom up here
-import React from "react";
-import { createRoot } from "react-dom/client";
+import * as React from 'react'
+import {createRoot} from 'react-dom/client'
+import {Logo} from './components/logo'
+import '@reach/dialog/styles.css'
+import Dialog from '@reach/dialog'
 
-// 🐨 you'll also need to import the Logo component from './components/logo'
-import { Logo } from "components/logo";
-// 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
-// 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
+function App() {
+    const [openModal, setOpenModal] = React.useState('none')
 
-const App = () => {
   return (
     <div>
-        <Logo/>
-        <h1>Bookshelf</h1>
-        <button onClick={() => alert('Login button was clicked')}>Login</button>
-        <button onClick={() => alert('Register button was clicked')}>Register</button>
+      <Logo width="80" height="80" />
+      <h1>Bookshelf</h1>
+      <div>
+        <button onClick={() => setOpenModal('login')}>Login</button>
+      </div>
+      <div>
+        <button onClick={() => setOpenModal('register')}>Register</button>
+      </div>
+      <Dialog isOpen={openModal === 'login'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+        <h3>Login</h3>
+      </Dialog>
+      <Dialog isOpen={openModal === 'register'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+        <h3>Register</h3>
+      </Dialog>
     </div>
-
   )
 }
 
-export default App
-
-// 🐨 use createRoot to render the <App /> to the root element
-// 💰 find the root element with: document.getElementById('root')
-
-
-const root = createRoot(
-    document.getElementById('root')
-  );
-  root.render(<App/>);
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
+export {root}
